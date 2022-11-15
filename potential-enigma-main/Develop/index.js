c93fa8e00dae2e9958dd1412c3134e52a6912c2a
kn7767 <112599359+kn7767@users.noreply.github.com>
2022-11-15 11:26:25 -0800

    added questions

---
+++ potential-enigma-main/Develop/index.js
@@ -0,0 +1,67 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+// Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+
+const questions = [
+    {
+        type: 'input', 
+        name: 'name',
+        message: "Welcome to the README generator. To start, provide your full name: "            
+        },
+    {
+        type: 'input',
+        name: 'github',
+        message: "Enter your Github username: "
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Enter your email: "
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: "Enter your project title: "
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "Enter your project description: "
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: "Enter instructions for installation: "
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: "Enter instructions for project usage: "
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: "Enter how can others contribute to this project: "
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: "Enter the tests written for your application: "
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+
+}
+
+// TODO: Create a function to initialize app
+function init() {
+
+}
+
+// Function call to initialize app
+init(
+
+);
