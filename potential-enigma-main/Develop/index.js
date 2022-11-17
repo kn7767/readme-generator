586f107ad56e805fb9eb9fb76c39223a0ab9a651
kn7767 <112599359+kn7767@users.noreply.github.com>
2022-11-16 21:20:35 -0800

    generates README in terminal, still needs ToC

--- potential-enigma-main/Develop/index.js
+++ potential-enigma-main/Develop/index.js
@@ -1,4 +1,12 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+// import inquirer from "inquirer";
+
+var generateMarkdown = require(`./utils/generateMarkdown`);
+// import generateMarkdown from './utils/generateMarkdown';
+
+const fs = require('fs');
+// import fs from "fs";
 
 // TODO: Create an array of questions for user input
 // Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
@@ -6,18 +14,42 @@
 const questions = [
     {
         type: 'input', 
-        name: 'name',
-        message: "Welcome to the README generator. To start, provide your full name: "            
-        },
+        name: 'fullName',
+        message: "Welcome to the README generator. To start, enter your full name: ",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your full name.');
+                return false;
+            }
+        }         
+    },
     {
         type: 'input',
         name: 'github',
-        message: "Enter your Github username: "
+        message: "Enter your Github username: ",
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username.');
+                return false;
+            }
+        }  
     },
     {
         type: 'input',
         name: 'email',
-        message: "Enter your email: "
+        message: "Enter your email: ",
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Please enter your email address.');
+                return false;
+            }
+        }
     },
     {
         type: 'input',
@@ -40,6 +72,11 @@ const questions = [
         message: "Enter instructions for project usage: "
     },
     {
+        type: 'list',
+        name: 'license',
+        choices: ['MIT', 'GNU', 'Apache', 'BSD', 'ISC']
+    },
+    {
         type: 'input',
         name: 'contribute',
         message: "Enter how can others contribute to this project: "
@@ -53,15 +90,37 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-
+    // let result = generateMarkdown(answers);
+    // console.log(result);
+    // fs.writeFile(fileName, data, error => {
+    //     if (error) throw error;
+    //     console.log('README has been created')
+    // })
 }
 
 // TODO: Create a function to initialize app
 function init() {
+    inquirer
+    .prompt(questions)
+    .then(function(answers) {
+        console.log(answers);
+
+        let result = generateMarkdown(answers)
+        console.log(result);
+
+    })
+    .then(answers => {
+        return generateMarkdown(answers);
+
+    })
+        .then(function(markdown) {
+        writeToFile('./dist/README.md', markdown)
+    })
+    .catch(function(error){
+        console.log(error);
+    })
 
 }
 
 // Function call to initialize app
-init(
-
-);
+init();
--- potential-enigma-main/Develop/utils/generateMarkdown.js
+++ potential-enigma-main/Develop/utils/generateMarkdown.js
@@ -1,20 +1,64 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (!license) {
+      return ``;
+    } else {
+      return `[![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(license)})`
+    }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'GPL') {
+    return `http://perso.crans.org/besson/LICENSE.html`
+  }
+  if (license === 'MIT') {
+    return `https://lbesson.mit-license.org/`
+  }
+  if (license === 'CC--0') {
+    return `https://creativecommons.org/licenses/by-nd/4.0` 
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return ``;
+  } else {
+    return ` ## License
+    This project is licensed by: ${license}`
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+##Description
+${data.description}
+
+##Table of Contents
+
+##Installation
+${data.install}
+
+##Usage
+${data.usage}
+
+##Contributing
+${data.contribute}
+
+##Tests
+${data.tests}
+
+##Questions
+If you have any questions, contact me here: ${data.email} 
+GitHub profile: github.com/${data.github}
+
+${renderLicenseBadge}
 `;
 }
-
 module.exports = generateMarkdown;
